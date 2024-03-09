1e4ba146622feb2b4264fee0933fb70d754358ba
Rashika258 <rashikasuresh5758@gmail.com>
2024-03-09 19:11:51 +0530

    -- resume changes

---
+++ app/components/FlexboxSpacer.tsx
@@ -0,0 +1,17 @@
+export const FlexboxSpacer = ({
+    maxWidth,
+    minWidth = 0,
+    className = "",
+  }: {
+    maxWidth: number;
+    minWidth?: number;
+    className?: string;
+  }) => {
+    return (
+      <div
+        className={`invisible shrink-[10000] grow ${className}`}
+        style={{ maxWidth: `${maxWidth}px`, minWidth: `${minWidth}px` }}
+      />
+    );
+  };
+  
---
+++ app/resume-parser/ResumeParserAlgorithmArticle.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ResumeParserAlgorithmArticle = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default ResumeParserAlgorithmArticle;
